380534acb489a6ea37de6136522834fca6992ec2
lavaemohale <lavaemohale@gmail.com>
2025-10-09 20:01:10 +0200

    Untrack backend node_modules, update vercel.json files

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,11 @@ const pool = new Pool({
 app.use(cors());
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../frontend/build/index.html')));
+}
+
 // Test connection on startup
 pool.connect((err, client, release) => {
   if (err) {
